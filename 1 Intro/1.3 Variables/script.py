3c97dd5b9041d67ce48428fd9f28db860545e5df
Carlos <carlosmiguelibarra01@gmail.com>
2025-09-21 21:41:25 -0700

    project

---
+++ 1 Intro/1.3 Variables/script.py
@@ -0,0 +1,3 @@
+name = input("whats your name ") # Store input in a variable
+length = len(name) # Get length of the input string
+print("Hello " + name + "! Your name is " + str(length) + " characters long.") # Concatenate strings with + operator
